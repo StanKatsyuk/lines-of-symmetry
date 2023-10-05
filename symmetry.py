dde908a7dda30078aa28c5f70b7371996050895b
Stanislav Katsyuk <stankatsyuk@Stanislavs-MacBook-Pro.local>
2023-10-05 13:59:13 -0700

    Add midpoint and slope functions

--- symmetry.py
+++ symmetry.py
@@ -28,3 +28,30 @@ class Point:
 
     def __hash__(self) -> int:
         return hash((self.x, self.y))
+
+
+def find_symmetry_lines(points: list[Point]) -> set[tuple[float, float]]:
+    if not points:
+        return set()
+
+    def midpoint(p1, p2):
+        """
+        Calculate the midpoint between two points.
+
+        :param p1: The first point.
+        :param p2: The second point.
+        :return: A new Point object representing the midpoint.
+        """
+        return Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
+
+    def slope(p1, p2):
+        """
+        Calculate the slope of the line passing through two points.
+
+        :param p1: The first point.
+        :param p2: The second point.
+        :return: The slope of the line or 'float('inf')' for a vertical line.
+        """
+        if p2.x - p1.x == 0:  # vertical line
+            return float('inf')
+        return (p2.y - p1.y) / (p2.x - p1.x)
