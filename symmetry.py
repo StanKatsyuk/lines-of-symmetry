53d6b18a5a3de5f266293a90a06ce4f0d2ba9c69
Stanislav Katsyuk <stankatsyuk@Stanislavs-MacBook-Pro.local>
2023-10-08 14:51:59 -0700

    Autofixes via black

--- symmetry.py
+++ symmetry.py
@@ -1,5 +1,5 @@
+PRECISION = 0.001
 
-PRECISION = .001
 
 class Point:
     """
@@ -9,11 +9,12 @@ class Point:
         x (float): The x-coordinate of the point.
         y (float): The y-coordinate of the point.
     """
+
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
-    def __eq__(self, other: 'Point') -> bool:
+    def __eq__(self, other: "Point") -> bool:
         return self.x == other.x and self.y == other.y
 
     def __hash__(self) -> int:
@@ -34,16 +35,16 @@ def find_symmetry_lines(points: list[Point]) -> set[tuple[float, float]]:
 
     num_points = len(points)
     if num_points <= 1 or not num_points:
-        raise ValueError('Please provide at least two points')
+        raise ValueError("Please provide at least two points")
     elif num_points > 100:
-        raise ValueError('Algorithm is not optimized to run with more than 100 points')
+        raise ValueError("Algorithm is not optimized to run with more than 100 points")
 
     def midpoint(p1, p2):
         return Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2)
 
     def slope(p1, p2):
         if p2.x - p1.x == 0:  # vertical line
-            return float('inf')
+            return float("inf")
         return (p2.y - p1.y) / (p2.x - p1.x)
 
     def perpendicular_bisector(p1: Point, p2: Point) -> tuple[float, float]:
@@ -51,8 +52,8 @@ def find_symmetry_lines(points: list[Point]) -> set[tuple[float, float]]:
         mid = midpoint(p1, p2)
 
         if m == 0:  # horizontal line
-            return (float('inf'), mid.x)
-        if m == float('inf'):  # vertical line
+            return (float("inf"), mid.x)
+        if m == float("inf"):  # vertical line
             return (0, mid.y)
 
         m_perpendicular = -1 / m
@@ -69,7 +70,7 @@ def find_symmetry_lines(points: list[Point]) -> set[tuple[float, float]]:
                 return False
         return True
 
-    # If all the points are collinear, return the perpendicular bisector 
+    # If all the points are collinear, return the perpendicular bisector
     # of the line formed by the first and last points as the only line of symmetry.
     if are_collinear(points):
         return {perpendicular_bisector(points[0], points[-1])}
@@ -83,22 +84,22 @@ def find_symmetry_lines(points: list[Point]) -> set[tuple[float, float]]:
 
     def is_close(p1: Point, p2: Point, tol=PRECISION) -> bool:
         return abs(p1.x - p2.x) < tol and abs(p1.y - p2.y) < tol
-    
+
     def reflect_point(m: float, c: float, p: Point) -> Point:
         # For vertical lines (infinite slope):
-        if m == float('inf'):
+        if m == float("inf"):
             # The x value of the reflected point will be symmetrically opposite
             # with respect to the line's x-intercept, which is the value 'c' here.
-            x_reflected = 2*c - p.x
+            x_reflected = 2 * c - p.x
             return Point(x_reflected, p.y)
-        
+
         A = m
         B = -1
         C = c
-        
+
         x_prime = p.x - 2 * A * (A * p.x + B * p.y + C) / (A**2 + B**2)
         y_prime = p.y - 2 * B * (A * p.x + B * p.y + C) / (A**2 + B**2)
-        
+
         return Point(x_prime, y_prime)
 
     # TODO(Stan): This should be optimized to avoid the present O(n^2) runtime
--- test_symmetry.py
+++ test_symmetry.py
@@ -1,12 +1,15 @@
-import pytest
 import math
 import random
+
+import pytest
+
 from symmetry import Point, find_symmetry_lines
 
 # Expected error messages
-EXPECTED_ERROR_MESSAGE = 'Please provide at least two points'
+EXPECTED_INPUT_LENGTH_MESSAGE = "Please provide at least two points"
 EXPECTED_LIST_ERROR = "points must be a list"
 EXPECTED_ITEM_ERROR = "Each item in the points list must be an instance of Point"
+
 
 def generate_large_input(num_points: int, symmetric: bool = True) -> list[Point]:
     """
@@ -20,68 +23,90 @@ def generate_large_input(num_points: int, symmetric: bool = True) -> list[Point]
     :return: A list of generated points.
     """
     half_num_points = num_points // 2
-    
+
     if symmetric:
         points = [Point(i, i) for i in range(half_num_points)]
         points.extend([Point(-i, -i) for i in range(half_num_points)])
         return points
     else:
-        return [Point(random.uniform(0, 1), random.uniform(0, 1)) for _ in range(num_points)]
+        return [
+            Point(random.uniform(0, 1), random.uniform(0, 1)) for _ in range(num_points)
+        ]
 
-@pytest.mark.parametrize("points, expected, shape", [
-    ([Point(0, 2), Point(-1, 0), Point(0, -2), Point(1, 0)], 2, 'Rhombus'),
-    ([Point(1, 1), Point(-1, 1), Point(1, -1), Point(-1, -1)], 4, 'Square'),
-    ([Point(0, 2), Point(-1, 0), Point(0, -1), Point(1, 0)], 1, 'Kite'),
-    ([Point(-2, 1), Point(2, 1), Point(1, -1), Point(-1, -1)], 1, 'Isosceles Trapezoid'),
-    ([Point(-2, 1), Point(2, 1), Point(1.5, -1), Point(-1.5, -1)], 1, 'Trapezoid'),
-    ([Point(2, 1), Point(0, 2), Point(-2, -1), Point(0, -2)], 0, 'Parallelogram'),
-    ([Point(0, math.sqrt(3)), Point(-1, 0), Point(1, -0)], 3, 'Equilateral Triangle'),
-    ([Point(0, 2), Point(-1, 0), Point(1, 0)], 1, 'Isosceles Triangle')
-])
+
+@pytest.mark.parametrize("input_points", [[], [Point(1, 1)]])
+def test_num_points_conditions(input_points):
+    with pytest.raises(ValueError, match=EXPECTED_INPUT_LENGTH_MESSAGE):
+        find_symmetry_lines(input_points)
+
+
+@pytest.mark.parametrize(
+    "input_points, expected_error",
+    [
+        (tuple([Point(1, 1), Point(2, 2)]), EXPECTED_LIST_ERROR),  # tuple instead of list
+        ({Point(1, 1), Point(2, 2)}, EXPECTED_LIST_ERROR),  # set instead of list
+        ("string", EXPECTED_LIST_ERROR),  # string instead of list
+        ([Point(1, 1), "not_a_point"], EXPECTED_ITEM_ERROR),  # one item is not a Point
+        ([1, 2, 3], EXPECTED_ITEM_ERROR),  # none of the items are Points
+    ],
+)
+def test_input_type_conditions(input_points, expected_error):
+    with pytest.raises(TypeError, match=expected_error):
+        find_symmetry_lines(input_points)
+
+
+def test_exceed_max_points():
+    points = generate_large_input(
+        101, False
+    )  # Generate 101 points which is above the limit
+    print(len(points))
+    with pytest.raises(
+        ValueError, match="Algorithm is not optimized to run with more than 100 points"
+    ):
+        find_symmetry_lines(points)
+
+
+@pytest.mark.parametrize(
+    "points, expected, shape",
+    [
+        ([Point(0, 2), Point(-1, 0), Point(0, -2), Point(1, 0)], 2, "Rhombus"),
+        ([Point(1, 1), Point(-1, 1), Point(1, -1), Point(-1, -1)], 4, "Square"),
+        ([Point(0, 2), Point(-1, 0), Point(0, -1), Point(1, 0)], 1, "Kite"),
+        ([Point(-2, 1), Point(2, 1), Point(1, -1), Point(-1, -1)], 1,"Isosceles Trapezoid",),
+        ([Point(-2, 1), Point(2, 1), Point(1.5, -1), Point(-1.5, -1)], 1, "Trapezoid"),
+        ([Point(2, 1), Point(0, 2), Point(-2, -1), Point(0, -2)], 0, "Parallelogram"),
+        ([Point(0, math.sqrt(3)), Point(-1, 0), Point(1, -0)], 3, "Equilateral Triangle",),
+        ([Point(0, 2), Point(-1, 0), Point(1, 0)], 1, "Isosceles Triangle"),
+    ],
+)
 def test_symmetry_of_shapes(points, expected, shape):
     lines = find_symmetry_lines(points)
-    assert len(lines) == expected, f"{shape} - lines expected: {expected} actual: {len(lines)}"
+    assert (
+        len(lines) == expected
+    ), f"{shape} - Expected lines: {expected}, Actual: {len(lines)}"
 
-@pytest.mark.parametrize("num_points, symmetric, expected_lines", [
-    (30, True, 1),
-    (60, True, 1),
-    (100, True, 1),
-    (30, False, 0),
-    (60, False, 0),
-    (100, False, 0),
-])
+
+@pytest.mark.parametrize(
+    "num_points, symmetric, expected_lines",
+    [
+        (30, True, 1),
+        (60, True, 1),
+        (100, True, 1),
+        (30, False, 0),
+        (60, False, 0),
+        (100, False, 0),
+    ],
+)
 def test_large_input(num_points, symmetric, expected_lines):
     points = generate_large_input(num_points, symmetric)
     result = len(find_symmetry_lines(points))
-    assert result == expected_lines, f"Expected {expected_lines} lines of symmetry for {num_points} points, but got {result}."
+    assert (
+        result == expected_lines
+    ), f"Expected {expected_lines} lines of symmetry for {num_points} points, but got {result}."
 
-def test_exceed_max_points():
-    points = generate_large_input(101, False)  # Generate 101 points which is above the limit
-    print(len(points))
-    with pytest.raises(ValueError, match='Algorithm is not optimized to run with more than 100 points'):
-        find_symmetry_lines(points)
 
 def test_collinear_points():
     collinear_points = [Point(1, 1), Point(2, 2), Point(3, 3), Point(4, 4)]
     expected_lines = 1
     lines = find_symmetry_lines(collinear_points)
     assert len(lines) == expected_lines
-
-@pytest.mark.parametrize("input_points", [
-    [],
-    [Point(1, 1)]
-])
-def test_num_points_conditions(input_points):
-    with pytest.raises(ValueError, match=EXPECTED_ERROR_MESSAGE):
-        find_symmetry_lines(input_points)
-
-@pytest.mark.parametrize("input_points, expected_error", [
-    (tuple([Point(1, 1), Point(2, 2)]), EXPECTED_LIST_ERROR),  # tuple instead of list
-    ({Point(1, 1), Point(2, 2)}, EXPECTED_LIST_ERROR),        # set instead of list
-    ("string", EXPECTED_LIST_ERROR),                          # string instead of list
-    ([Point(1, 1), "not_a_point"], EXPECTED_ITEM_ERROR),      # one item is not a Point
-    ([1, 2, 3], EXPECTED_ITEM_ERROR)                           # none of the items are Points
-])
-def test_input_type_conditions(input_points, expected_error):
-    with pytest.raises(TypeError, match=expected_error):
-        find_symmetry_lines(input_points)
