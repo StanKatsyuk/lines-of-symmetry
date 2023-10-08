ac43a812a8a79ebc485c23edfb7b63c3e4443483
Stanislav Katsyuk <stankatsyuk@Stanislavs-MacBook-Pro.local>
2023-10-08 14:10:39 -0700

    Fix tests

--- test_symmetry.py
+++ test_symmetry.py
@@ -1,4 +1,6 @@
 import pytest
+import math
+import random
 from symmetry import Point, find_symmetry_lines
 
 
@@ -20,23 +22,21 @@ def generate_large_input(num_points: int, symmetric: bool = True) -> list[Point]
         points.extend([Point(-i, -i) for i in range(half_num_points)])
         return points
     else:
-        return [Point(i, i) for i in range(num_points)]
-
+        return [Point(random.uniform(0, 1), random.uniform(0, 1)) for _ in range(num_points)]
 
 @pytest.mark.parametrize("points, expected, shape", [
     ([Point(0, 2), Point(-1, 0), Point(0, -2), Point(1, 0)], 2, 'Rhombus'),
     ([Point(1, 1), Point(-1, 1), Point(1, -1), Point(-1, -1)], 4, 'Square'),
     ([Point(0, 2), Point(-1, 0), Point(0, -1), Point(1, 0)], 1, 'Kite'),
     ([Point(-2, 1), Point(2, 1), Point(1, -1), Point(-1, -1)], 1, 'Isosceles Trapezoid'),
-    ([Point(-2, 1), Point(2, 1), Point(1.5, -1), Point(-1.5, -1)], 0, 'Trapezoid'),
+    ([Point(-2, 1), Point(2, 1), Point(1.5, -1), Point(-1.5, -1)], 1, 'Trapezoid'),
     ([Point(2, 1), Point(0, 2), Point(-2, -1), Point(0, -2)], 0, 'Parallelogram'),
-    ([Point(0, 1.73), Point(-1, -0.87), Point(1, -0.87)], 3, 'Equilateral Triangle'),
+    ([Point(0, math.sqrt(3)), Point(-1, 0), Point(1, -0)], 3, 'Equilateral Triangle'),
     ([Point(0, 2), Point(-1, 0), Point(1, 0)], 1, 'Isosceles Triangle')
 ])
 def test_symmetry_of_shapes(points, expected, shape):
     lines = find_symmetry_lines(points)
     assert len(lines) == expected, f"{shape} - lines expected: {expected} actual: {len(lines)}"
-
 
 @pytest.mark.parametrize("num_points, symmetric, expected_lines", [
     (30, True, 1),
@@ -51,19 +51,11 @@ def test_large_input(num_points, symmetric, expected_lines):
     result = len(find_symmetry_lines(points))
     assert result == expected_lines, f"Expected {expected_lines} lines of symmetry for {num_points} points, but got {result}."
 
-
-def _test_symmetry(num_points, expected_lines):
-    points = generate_large_input(num_points, symmetric=(expected_lines > 0))
-    result = len(find_symmetry_lines(points))
-    assert result == expected_lines, f"Expected {expected_lines} lines of symmetry for {num_points} points, but got {result}."
-
-
 def test_exceed_max_points():
     points = generate_large_input(101, False)  # Generate 101 points which is above the limit
     print(len(points))
     with pytest.raises(ValueError, match='Algorithm is not optimized to run with more than 100 points'):
         find_symmetry_lines(points)
-
 
 def test_collinear_points():
     collinear_points = [Point(1, 1), Point(2, 2), Point(3, 3), Point(4, 4)]
